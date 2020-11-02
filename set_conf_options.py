02e39b34ba9a818566643eb82c98215f5261f43f
adm5 <alex.delgado@embention.com>
2020-11-02 16:18:09 +0100

    embention/RTD#4 - change in get title

--- set_conf_options.py
+++ set_conf_options.py
@@ -7,8 +7,8 @@ def get_project_title(path_index):
     with open(os.path.join(path_index,"index.rst"), "r", encoding="utf-8") as index_file:
         previous_line = ""
         for line in index_file.readlines():
-            if "*****" in line or "######" in line or "=======" in line:
-                if ".." not in previous_line[0:2] and not previous_line[0:1].isspace() and "###" not in previous_line and "***" not in previous_line and not previous_line.isspace() and "===" not in previous_line and previous_line:
+            if "****" in line or "####" in line or "====" in line:
+                if ".." not in previous_line[0:2] and not previous_line[0:1].isspace() and "####" not in previous_line and "****" not in previous_line and not previous_line.isspace() and "====" not in previous_line and previous_line:
                     return ' '.join(previous_line.replace("\n","").replace("\t","").split())
             previous_line = line
     return ""
